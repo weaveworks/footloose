98976beff02e786c239e8b2c6a9d9227b81a587d
Damien Lespiau <damien@weave.works>
2019-02-25 08:53:07 +0000

    tests: Explicitly ask for the centos7 image in the centos7 tests
    It's possible we'll change the default from cento7 to fedora 29. Guard the
    centos7 tests from possible future changes.

--- tests/test-create-delete-centos7.cmd
+++ tests/test-create-delete-centos7.cmd
@@ -1,4 +1,4 @@
-footloose config create --config %t.footloose --name %t --key %t-key
+footloose config create --config %t.footloose --name %t --key %t-key --image quay.io/footloose/centos7
 footloose create --config %t.footloose
 %out docker ps --format {{.Names}}
 footloose delete --config %t.footloose
