1d6e7ed5622d864b8b6a7485307e97e447916966
Damien Lespiau <damien@weave.works>
2019-03-01 12:39:02 +0000

    build: Improve make-image to tag and push OS base images

--- make-image.sh
+++ make-image.sh
@@ -3,18 +3,43 @@
 set -euo pipefail
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+org=quay.io/footloose
 
-if [ $# != 1 ]; then
-  echo Usage: make-image.sh IMAGE
+if [ $# -lt 2 ]; then
+  echo "Usage: make-image.sh VERB IMAGE [ARGS]"
   echo
-  echo Image is one of:
-    echo "  • all (build all images)"
+  echo VERB is one of:
+  echo "  • build"
+  echo "  • tag"
+  echo "  • push"
+  echo
+  echo IMAGE is one of:
+  echo "  • all (build all images)"
   for d  in `ls $scriptdir/images`; do
     echo "  • $d"
   done
+  echo
+  echo "ARGS are VERB-specific optional arguments"
+  echo "  • tag requires a version"
+  echo "  • push takes an option version, defaults to 'latest' "
+  echo
+  echo "Examples:"
+  echo "  • $0 build all"
+  echo "  • $0 tag 0.2.0"
+  echo "  • $0 push 0.2.0"
+  exit 1
 fi
 
-images=$1
+verb=$1
+case $verb in
+build|tag|push)
+  ;;
+*)
+  echo "error: unknown verb '$verb'"
+  exit 1
+esac
+
+images=$2
 if [ "$images" == "all" ]; then
   images=""
   for d  in `ls $scriptdir/images`; do
@@ -22,6 +47,37 @@ if [ "$images" == "all" ]; then
   done
 fi
 
-for image in $images; do
-  (cd $scriptdir/images/$image && docker build -t quay.io/footloose/$image .)
-done
+shift
+shift
+
+case $verb in
+
+build)
+  for image in $images; do
+    echo "  • Building $org/$image"
+    (cd $scriptdir/images/$image && docker build -t $org/$image .)
+  done
+  ;;
+
+tag)
+  if [ $# != 1 ]; then
+    echo "error: usage tag IMAGE VERSION"
+    exit 1
+  fi
+  version=$1
+  for image in $images; do
+    echo "  • Tagging $org/$image:$version"
+    docker tag $org/$image:latest $org/$image:$version
+  done
+  ;;
+
+push)
+  version=latest
+  [ -n "$1" ] && version=$1
+  for image in $images; do
+    echo "  • Pushing $org/$image:$version"
+    docker push $org/$image:$version
+  done
+  ;;
+
+esac
