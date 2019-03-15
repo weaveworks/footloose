119bf81451a41bf3c4478ffc3d5c4292fbcd8bab
Henk <hbokhoven@gmail.com>
2019-03-15 11:00:05 +0100

    Add a test running docker in docker with debian10

---
+++ tests/test-docker-in-docker-debian10.cmd
@@ -0,0 +1,6 @@
+footloose create --config %testName.yaml
+footloose --config %testName.yaml ssh root@node0 -- apt update && apt install -y docker.io
+footloose --config %testName.yaml ssh root@node0 systemctl start docker
+footloose --config %testName.yaml ssh root@node0 docker pull busybox
+%out footloose --config %testName.yaml ssh root@node0 docker run busybox echo success
+footloose delete --config %testName.yaml
