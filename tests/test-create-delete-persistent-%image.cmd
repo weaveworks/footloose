553817efbe5764a6a378a6b1ce69164daa78b7cf
CarlosEDP <me@carlosedp.com>
2019-03-29 22:16:23 -0300

    Add tests for persistent containers and start/stop

---
+++ tests/test-create-delete-persistent-%image.cmd
@@ -0,0 +1,6 @@
+footloose config create --config %testName.footloose --name %testName --persistent true --key %testName-key --image quay.io/footloose/%image
+footloose create --config %testName.footloose
+%out docker ps --format {{.Names}}
+%out docker inspect %testName-node0 -f "{{.HostConfig.AutoRemove}}"
+footloose delete --config %testName.footloose
+%out docker ps --format {{.Names}}
---
+++ tests/test-create-stop-start-%image.cmd
@@ -0,0 +1,8 @@
+footloose config create --config %testName.footloose --name %testName --persistent true --key %testName-key --image quay.io/footloose/%image
+footloose create --config %testName.footloose
+%out docker ps --format {{.Names}}
+footloose stop --config %testName.footloose
+%out docker inspect %testName-node0 -f "{{.State.Running}}"
+footloose start --config %testName.footloose
+%out docker inspect %testName-node0 -f "{{.State.Running}}"
+footloose delete --config %testName.footloose
