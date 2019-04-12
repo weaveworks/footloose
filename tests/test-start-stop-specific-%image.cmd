0c60d40668018234190a2aa567449d51fc96ec15
nathanh <nathan.haim@free.fr>
2019-04-12 19:00:55 +0200

    Cluster Start Stop
    This fixes specific start stop tests format

--- tests/test-start-stop-specific-%image.cmd
+++ tests/test-start-stop-specific-%image.cmd
@@ -1,4 +1,4 @@
-footloose config create --config %testName.footloose --name %testName --key %testName-key --image quay.io/footloose/%image
+footloose config create --config %testName.footloose --name %testName --key %testName-key --image quay.io/footloose/%image --replicas 3
 footloose create --config %testName.footloose
 footloose stop %testName-node1 --config %testName.footloose
 %out docker inspect %testName-node0 -f "{{.State.Running}}"
