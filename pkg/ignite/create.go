a90cf3ef0aabc2760a1131626180fb3f22d24d56
Dennis Marttinen <dennis@weave.works>
2019-08-19 20:31:53 +0300

    Ignite backend: use ephemeral ports if no hostPort is given

--- pkg/ignite/create.go
+++ pkg/ignite/create.go
@@ -2,6 +2,7 @@ package ignite
 
 import (
 	"fmt"
+	"net"
 
 	"github.com/weaveworks/footloose/pkg/config"
 	"github.com/weaveworks/footloose/pkg/exec"
@@ -34,12 +35,16 @@ func Create(name string, spec *config.Machine, pubKeyPath string) (id string, er
 		runArgs = append(runArgs, v)
 	}
 
-	for i, mapping := range spec.PortMappings {
+	for _, mapping := range spec.PortMappings {
 		if mapping.HostPort == 0 {
-			// TODO: should warn here as containerPort is dropped
-			continue
+			// If not defined, set the host port to a random free ephemeral port
+			var err error
+			if mapping.HostPort, err = freePort(); err != nil {
+				return "", err
+			}
 		}
-		runArgs = append(runArgs, fmt.Sprintf("--ports=%d:%d", int(mapping.HostPort)+i, mapping.ContainerPort))
+
+		runArgs = append(runArgs, fmt.Sprintf("--ports=%d:%d", int(mapping.HostPort), mapping.ContainerPort))
 	}
 
 	_, err = exec.ExecuteCommand(execName, runArgs...)
@@ -56,9 +61,26 @@ func setupCopyFiles(copyFiles map[string]string) []string {
 }
 
 func IsCreated(name string) bool {
-	exitCode, err := exec.ExecForeground(execName, "logs", name)
-	if err != nil || exitCode != 0 {
+	_, err := exec.ExecuteCommand(execName, "logs", name)
+	if err != nil {
 		return false
 	}
 	return true
+}
+
+// freePort requests a free/open ephemeral port from the kernel
+// Heavily inspired by https://github.com/phayes/freeport/blob/master/freeport.go
+func freePort() (uint16, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+
+	return uint16(l.Addr().(*net.TCPAddr).Port), nil
 }
